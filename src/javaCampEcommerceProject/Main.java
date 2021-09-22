3234f9b62faa1d678cff0fc6900110171ab5d477
unknown <besb-5834@hotmail.com>
2021-09-22 15:05:23 +0300

    second

--- src/javaCampEcommerceProject/Main.java
+++ src/javaCampEcommerceProject/Main.java
@@ -11,12 +11,12 @@ import javaCampEcommerceProject.entities.concretes.User;
 public class Main {
 
 	public static void main(String[] args) {
-		User user1 = new User(1,"Çağrı","AKINCI","cagri.akinci@outlook.com","123456");
-		User user2 = new User(2,"Çağrı","AKINCI","abc.com","123456"); // E-posta hatası verir.
-		User user3 = new User(3,"Ç","AKINCI","cagri.akinci@outlook.com","123456"); // isim hatası verir.
-		User user4 = new User(4,"Çağrı","A","cagri.akinci@outlook.com","123456"); // soyisim hatası verir.
-		User user5 = new User(5,"Çağrı","AKINCI","cagri.akinci@outlook.com","123"); // þifre hatası verir.
-		User user6 = new User(6,"Çağrı","AKINCI","abcfsdas@asdgmail.com","123456"); // gmail hatası verir.
+		User user1 = new User(1,"Buse","Yalçın","buse.yalcin@gmail.com","123456");
+		User user2 = new User(2,"Buse","Yalçın","abc.com","123456"); // E-posta hatası verir.
+		User user3 = new User(3,"B","Yalçın","buse.yalcin@gmail.com","123456"); // isim hatası verir.
+		User user4 = new User(4,"Buse","Y","buse.yalcin@gmail.com","123456"); // soyisim hatası verir.
+		User user5 = new User(5,"Buse","Yalçın","buse.yalcin@gmail.com","123"); // þifre hatası verir.
+		User user6 = new User(6,"Buse","Yalçın","abcfsdas@asdgmail.com","123456"); // gmail hatası verir.
 		
 		UserManager userManager = new UserManager(new HibernateUserDao(),new MailCheckManager(),new UserCheckManager(), new GoogleManagerAdapter(), new GmailCheckManager());
 		userManager.signUp(user1);
